d63cf2465474deb6ceb947c1d3a5516a33197633
didimuniirah <157588525+didimuniirah@users.noreply.github.com>
2024-01-31 22:40:50 -0800

    added exercise.py files

--- shop.py
+++ shop.py
@@ -1,4 +1,8 @@
-item_taken=input("Enter item taken")
-number_of_items_taken=input("Enter number of items taken")
-price_of_item_taken=input("Enter price of item taken")
+item_taken=input("Enter item taken:")
+number_of_items_taken=int(input("Enter number of items taken:"))
 
+price_of_item_taken=int(input("Enter price of items taken:"))
+
+compute_amount=(number_of_items_taken * price_of_item_taken)
+
+print(f"please pay our {compute_amount}")
