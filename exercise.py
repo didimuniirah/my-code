ab392767997751568ce919795f109590122ed412
didimuniirah <157588525+didimuniirah@users.noreply.github.com>
2024-02-05 09:32:41 -0800

    added more_strings.py files

--- exercise.py
+++ exercise.py
@@ -1,6 +1,8 @@
 student_name=input("Enter your name: ")
 course_work_mark=int(input("Enter your course work mark: "))
 exam_score=int(input("Enter your exam mark: "))
-compute_exam_mark=(exam_score /100 * 70)
+
+compute_exam_mark=(exam_score /100 )* 70
 compute_final_score=(compute_exam_mark + course_work_mark)
+
 print(f"your exam mark is {compute_exam_mark}\n your final mark is {compute_final_score}")
---
+++ more_strings.py
@@ -0,0 +1,5 @@
+my_string="hello world"
+         #012345678
+print(my_string[6])   
+#slicing:start_index: end_index  
+print(my_string[3:7])  
