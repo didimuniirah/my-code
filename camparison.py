024125ae19838b21faa2cb708ee2130f3a2ae269
didimuniirah <157588525+didimuniirah@users.noreply.github.com>
2024-02-08 09:15:05 -0800

    added sumation.py

---
+++ camparison.py
@@ -0,0 +1,6 @@
+#program about comparison
+x=10
+y=19
+print(x==y)
+print(f"x is smaller than y then answer will be {x>y} ")
+print(x!=y)
---
+++ operators.py
@@ -0,0 +1,5 @@
+# a program to demonstrate the use of operators
+number_one=10
+number_two=19
+sum=number_one+number_two
+print(sum)
